73855984f340720fcb4afdf55260921ace2568be
cademarks <DickensConklinMisurda2332>
2021-11-02 18:28:02 -0700

    Fixed exception catching

--- Translator.java
+++ Translator.java
@@ -4,6 +4,7 @@
 
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 
 public class Translator{
@@ -28,19 +29,24 @@ public class Translator{
     }
 
     public static void translate(String pathName){
-        Scanner input = new Scanner(new File(pathName));
         String baseName = pathName.substring(0, pathName.length()-4);
         String className = getClassName(baseName);
         File file = new File(baseName + ".java");
-        file.createNewFile();
-        FileWriter output = new FileWriter(file);
-        output.write("public class " + className + " {\n");
-        output.write("public static void main(String[] ARGS) {\n");
-        while (input.hasNextLine()){
-            // TODO
-        }
-        output.write("}\n}\n");
-        output.close();
-        input.close();
+        try {
+            Scanner input = new Scanner(new File(pathName));
+            file.createNewFile();
+            FileWriter output = new FileWriter(file);
+            output.write("public class " + className + " {\n");
+            output.write("public static void main(String[] ARGS) {\n");
+            while (input.hasNextLine()){
+                // TODO
+            }
+            output.write("}\n}\n");
+            output.close();
+            input.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }        
     }
 }
