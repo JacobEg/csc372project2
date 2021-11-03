b0250d6cb073618909dc2eb755b0e3cb4c0cbe0c
Jacob Egestad <jacob.egestad@live.com>
2021-11-02 17:28:59 -0700

    Added skeleton for program

---
+++ Translator.java
@@ -0,0 +1,46 @@
+/*
+
+*/
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+
+public class Translator{
+    public static void main(String[] args){
+        if(args.length == 0){
+            System.out.println("Provide file to translate via command-line arg.");
+            return;
+        }
+        translate(args[0]);
+    }
+
+    public static String getClassName(String baseName){
+        int slashIndex = baseName.lastIndexOf('/');
+        int backSlashIndex = baseName.lastIndexOf('\\');
+        if(slashIndex != -1){
+            return baseName.substring(slashIndex + 1);
+        } else if (backSlashIndex != -1){
+            return baseName.substring(backSlashIndex + 1);
+        } else{
+            return baseName;
+        }
+    }
+
+    public static void translate(String pathName){
+        Scanner input = new Scanner(new File(pathName));
+        String baseName = pathName.substring(0, pathName.length()-4);
+        String className = getClassName(baseName);
+        File file = new File(baseName + ".java");
+        file.createNewFile();
+        FileWriter output = new FileWriter(file);
+        output.write("public class " + className + " {");
+        output.write("public statis void main(String[] ARGS) {");
+        while (input.hasNextLine()){
+            // TODO
+        }
+        output.write("}\n}");
+        output.close();
+        input.close();
+    }
+}
