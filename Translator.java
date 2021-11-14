e2fc642b23d640e6cfc0c21ebfc9ef915ad8b922
Jacob Egestad <jacob.egestad@live.com>
2021-11-14 14:06:01 -0700

    added some regex patterns

--- Translator.java
+++ Translator.java
@@ -1,13 +1,25 @@
 /*
-
+Authors: Jacob Egestad & Cade Marks
+File: Translator.java
+Project: Project 2
+Description: Translates a .txt file in our programming language from the command line to java
 */
 
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.util.regex.*;
+import java.util.HashMap;
 
 public class Translator{
+    // maps vars to their type (str, int, bool)
+    private static HashMap<String, String> vars = new HashMap<String, String>();
+    private static Matcher matcher;
+    // look into expressions; recursive regex?
+    private static Pattern varDecl = Pattern.compile("int\\s+[a-zA-Z]+[0-9_]*\\s*=\\s*-?[0-9]+|bool\\s+[a-zA-Z]+[0-9_]*\\s*=\\s*(true|false)|str\\s+[a-zA-Z]+[0-9_]*\\s*=\\s*(\".*\"|'.*')");
+    private static Pattern varAssgmt = Pattern.compile("[a-zA-Z]+[0-9_]*\\s*=\\s*(-?[0-9]+|(true|false)|(\".*\"|'.*'))");
+    private static Pattern print = Pattern.compile("print\\((\".*\"|'.*')\\)|print\\([a-zA-Z]+[0-9_]*\\)");
+
     public static void main(String[] args){
         if(args.length == 0){
             System.out.println("Provide file to translate via command-line arg.");
@@ -39,14 +51,15 @@ public class Translator{
             output.write("public class " + className + " {\n");
             output.write("public static void main(String[] ARGS) {\n");
             while (input.hasNextLine()){
-                // TODO
+                String line = input.nextLine();
+                // TODO: analyze line-by-line (how to do conditionals and loops)?
             }
-            output.write("}\n}\n");
+            output.write("\n}\n}\n");
             output.close();
             input.close();
         } catch (Exception e) {
             System.out.println(e);
             System.exit(0);
-        }        
+        }
     }
 }
