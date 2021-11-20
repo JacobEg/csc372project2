7a6214103499a9dfd3381f772773eea76716d46a
Jacob Egestad <jacob.egestad@live.com>
2021-11-20 16:19:53 -0700

    fix error with not matching int comp

---
+++ test2.java
@@ -0,0 +1,12 @@
+public class test2 {
+public static void main(String[] fin) {
+int[] ARGS = new int[fin.length];
+for(int i = 0; i < fin.length; i++){
+ARGS[i] = Integer.parseInt(fin[i]);
+}
+if (5 < 9) {
+System.out.print("ah\n");
+}
+
+}
+}
